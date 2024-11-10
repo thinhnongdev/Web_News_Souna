8bd4b50437f0a940a7dc564648eb5e98ab89f900
thinhnongdev <tienthinhkk@gmail.com>
2024-11-10 22:14:15 +0700

    create project

---
+++ src/test/java/com/example/web_news/WebNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

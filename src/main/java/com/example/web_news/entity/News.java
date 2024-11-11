acddb2ec87213a222f02844f6c5a2581c22a17b9
thinhnongdev <tienthinhkk@gmail.com>
2024-11-12 00:08:23 +0700

    update news entity

--- src/main/java/com/example/web_news/entity/News.java
+++ src/main/java/com/example/web_news/entity/News.java
@@ -1,10 +1,30 @@
 package com.example.web_news.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "news")
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class News {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     String id;
+    String code;
     String title;
+    String short_description;
+    String thumbnail;
     String description;
     String content;
-    String date;
-    String hehe;
+    LocalDateTime create_date;
+    LocalDateTime modified_date;
+    String create_by;
+    String modified_by;
+    Boolean status;
+
 }
